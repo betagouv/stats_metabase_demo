f8679c35f26b61acb1b11b679eac615266e05da5
Pieterjan Montens <pieterjan@montens.net>
2020-09-03 17:22:42 +0200

    :wrench: Tracking pathname is more interesting

--- demo_website/js/demo_init.js
+++ demo_website/js/demo_init.js
@@ -2,13 +2,15 @@
  * Exemple d'initialisation et d'utilisation du tracker javascript
  */
 document.addEventListener('DOMContentLoaded', function() {
-    track(window.location.href, Steps.LOAD);
+    // Option : use window.location.href to include domain
+    // location.hash and location.search are other interesting data points
+    track(window.location.pathname, Steps.LOAD);
 }, false);
 
 document.addEventListener('scroll', function() {
-    track(window.location.href, Steps.SCROLL);
+    track(window.location.pathname, Steps.SCROLL);
 }, false);
 
 document.addEventListener('click', function() {
-    track(window.location.href, Steps.CLICK);
+    track(window.location.pathname, Steps.CLICK);
 }, false);
